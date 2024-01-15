0b4ebe06c5bb21397ffeea448222f0ca9784a7c2
Kirill I <54040921+kirillivan0v@users.noreply.github.com>
2024-01-15 13:34:42 +0200

    Remove unseless things

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite';
+import * as path from 'path';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@styles': path.resolve(__dirname, './src/assets/styles')
+    }
+  },
   base: './',
   build: {
     outDir: 'build',
